9fa845077a57fa11c2cbb5b34152a5e010a42d4f
Sean <sjgallen@gmail.com>
2017-03-18 19:48:36 -0500

    fetched todays date from New York Times

--- neatJavasctiptFeatures/fetching.js
+++ neatJavasctiptFeatures/fetching.js
@@ -1,9 +1,9 @@
 var fetch = require('node-fetch');
 
-fetch('https://www.google.com', {
+fetch('https://www.nytimes.com', {
     method: 'get'
     }).then(function(response) {
-  console.log(response);
+  console.log(response.headers._headers.date);
 }).catch(function(err) {
       console.log(err);
     });

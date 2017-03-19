2bd16b4c65bb946449d50307f3501a5b6ea05511
Sean <sjgallen@gmail.com>
2017-03-18 19:49:26 -0500

    improved the date

--- neatJavasctiptFeatures/fetching.js
+++ neatJavasctiptFeatures/fetching.js
@@ -3,7 +3,7 @@ var fetch = require('node-fetch');
 fetch('https://www.nytimes.com', {
     method: 'get'
     }).then(function(response) {
-  console.log(response.headers._headers.date);
+  console.log(response.headers._headers.date[0]);
 }).catch(function(err) {
       console.log(err);
     });
